aa62b28e333bc1e31824302a03339a0f9473cc44
Alan Liu <alanl200511@gmail.com>
2026-07-04 15:57:21 -0400

    Adapting to comment #1

--- components/teams/edit-team-form.tsx
+++ components/teams/edit-team-form.tsx
@@ -26,24 +26,26 @@ export function EditTeamForm({
     useTeamForm({ orgId: ORG_ID, team });
 
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 lg:grid lg:grid-cols-[3fr_5fr] lg:items-start">
       <form
         id={formId}
         onSubmit={handleSubmit}
         noValidate
-        className="flex max-w-lg flex-col"
+        className="flex max-w-lg flex-col lg:col-start-1 lg:row-start-1"
       >
         <h2 className="text-lg font-semibold">Team settings</h2>
         <p className="mt-1 text-xs text-muted-foreground">* Required</p>
         <div className="mt-2 flex max-w-lg flex-col gap-6">{fields}</div>
       </form>
-      <MemberEditTable
-        memberships={memberships}
-        teamId={team.id}
-        teamSlug={team.slug}
-        currentRole={currentRole}
-      />
-      <div className="flex max-w-lg items-center gap-2">
+      <div className="lg:col-start-2 lg:row-span-2 lg:row-start-1">
+        <MemberEditTable
+          memberships={memberships}
+          teamId={team.id}
+          teamSlug={team.slug}
+          currentRole={currentRole}
+        />
+      </div>
+      <div className="flex max-w-lg items-center gap-2 lg:col-start-1 lg:row-start-2">
         {canDelete && (
           <DeleteTeamButton
             teamId={team.id}
